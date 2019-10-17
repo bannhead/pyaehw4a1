973f8d4fdffe08d82745d33dc852e3a339acdd5b
Davide Varricchio <davide.varricchio@gmail.com>
2019-10-17 17:04:54 +0200

    v0.0.4 implemented universal support for other packets type

--- pyaehw4a1/__main__.py
+++ pyaehw4a1/__main__.py
@@ -3,6 +3,7 @@ import json
 
 from . import aehw4a1
 from .commands import UpdateCommand
+from .commands import ReadCommand
 
 def main():
     parser = argparse.ArgumentParser("aehw4a1")
@@ -13,18 +14,16 @@ def main():
 
     client = aehw4a1.AehW4a1(args.host)
 
-    if args.command is not None:
-        if args.command in UpdateCommand.__dict__:
-            parsed = json.loads(client.command(args.command))
-            print("AC",args.host,args.command,":",json.dumps(parsed, indent=4, sort_keys=False))
-        else:
-            raise Exception("Unknown command: {0}".format(args.command))
-        #print("AC",args.host,args.command,":",client.command(args.command))
+    if args.command is None:
+        command = "status_102_0"
     else:
-        result = client.read_all()
-        for key in result.keys():
-            parsed = json.loads(result.get(key))
-            print("{0}: {1}".format(key, json.dumps(parsed, indent=4, sort_keys=False)))
+        command = args.command
+    
+    if command in ReadCommand.__dict__ or command in UpdateCommand.__dict__:
+        parsed = json.loads(client.command(command))
+        print("AC",args.host,command,":\n",json.dumps(parsed, indent=4, sort_keys=False))
+    else:
+        raise Exception("Unknown command: {0}".format(command))
 
 if __name__ == "__main__":
     main()
--- pyaehw4a1/aehw4a1.py
+++ pyaehw4a1/aehw4a1.py
@@ -6,58 +6,46 @@ from socket import SOCK_STREAM
 from .commands import ReadCommand
 from .commands import UpdateCommand
 from .responses import ResponsePacket
-from .responses import Data_102_0
+from .responses import DataPacket
 
 class AehW4a1:
     def __init__(self, host):
         self._host = host
 
     def command(self, command):
-        if  isinstance(command, str):
-            for pointer_to_enum in ReadCommand:
-                if command == pointer_to_enum.name:
-                    return self._read_command(pointer_to_enum, socket)
-            for pointer_to_enum in UpdateCommand:
-                if command == pointer_to_enum.name:
-                    return self._update_command(pointer_to_enum, socket)
-        else:   # call from read_all()
-            if command.name in ReadCommand.__dict__:
-                return self._read_command(command, socket)
+        for name, member in ReadCommand.__members__.items():
+            if command == name:
+            
+                return self._read_command(member, socket)
+        
+        for name, member in UpdateCommand.__members__.items():
+            if command == name:
+            
+                return self._update_command(member, socket)
 
-            if command.name in UpdateCommand.__dict__:
-                return self._update_command(command, socket)
-
-        raise Exception("Not yet implemented")
-
-    def read_all(self):
-        result = {}
-
-        for command in ReadCommand:
-            binary_string = self.command(command)    
-            result[command] = binary_string
-
-        return result
+        raise Exception("Not yet implemented: {0}".format(command))
 
     def _update_command(self, command, socket):
-        bytes_string = self._send_recv_packet(command, socket)
-                
-        compare_length = (len(ResponsePacket.correct_101_0.value))
-        if bytes_string[:int(compare_length)] != ResponsePacket.correct_101_0.value:
-            raise Exception("Wrong 101_0 response")
-            
-        return self.command(ReadCommand.status_102_0)
+        pure_bytes = self._send_recv_packet(command, socket)
+        packet_type = self._packet_type(pure_bytes)
+        
+        if self._check_response(packet_type, pure_bytes):
+        
+            return self.command("status_102_0")
+        
+        raise Exception("Unknown packet type: {0}".format(packet_type))
 
     def _read_command(self, command, socket):
-        bytes_string = self._send_recv_packet(command, socket)
-        
-        compare_length = (len(ResponsePacket.correct_102_0.value))
-        if bytes_string[:int(compare_length)] != ResponsePacket.correct_102_0.value:
-            raise Exception("Wrong 102_0 response")
+        pure_bytes = self._send_recv_packet(command, socket)
+        packet_type = self._packet_type(pure_bytes)
+        data_start_pos = self._check_response(packet_type, pure_bytes)
+        print(data_start_pos)
+        if data_start_pos:
+            result = self._bits_value(packet_type, pure_bytes, data_start_pos)
             
-        binary_string = "{:08b}".format(int(bytes_string.hex(),16))
-        result = self._bits_value(binary_string[(compare_length * 8):])
-        
-        return result
+            return result
+
+        raise Exception("Unknown packet type {0}: {1}".format(packet_type, pure_bytes.hex()))
 
     def _send_recv_packet(self, command, socket):
         with socket(AF_INET, SOCK_STREAM) as s:
@@ -68,11 +56,35 @@ class AehW4a1:
             
         return result
     
-    def _bits_value(self, string):
+    def _bits_value(self, packet_type, pure_bytes, data_pos):
         result = {}
-
-        for field in Data_102_0:
-            result[field.name] = string[(field.offset - 1):(field.offset + field.length - 1)]
         
-        return json.dumps(result)
+        binary_string = "{:08b}".format(int(pure_bytes.hex(),16))
+        binary_data = binary_string[data_pos*8:-24]
+        
+        for data_packet in DataPacket:
+            if packet_type in data_packet.name:
+                for field in data_packet.value:
+                    result[field.name] = binary_data[(field.offset - 1):(field.offset + field.length - 1)]
 
+                return json.dumps(result)
+                
+        raise Exception("Unknown data type {0}: {1}".format(packet_type, binary_data))     
+
+    def _packet_type(self, string):
+        type = int(string[13:14].hex(),16)
+        sub_type = int(string[14:15].hex(),16)
+        
+        result = "{0}_{1}".format(type, sub_type)
+        
+        return result
+        
+    def _check_response(self, packet_type, pure_bytes):
+        for response_packet in ResponsePacket:
+            if packet_type in response_packet.name:
+                if response_packet.value not in pure_bytes:
+                    raise Exception("Wrong response for type {0}: {1}".format(packet_type, pure_bytes.hex()))
+                
+                return len(response_packet.value)
+        
+        return False
