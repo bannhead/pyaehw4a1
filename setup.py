fd4383cbe4b85c0aa465ea7539a291b4c97d90dd
Davide Varricchio <davide.varricchio@gmail.com>
2019-10-19 11:34:53 +0200

    Some adjustment for publish on pypi.org

--- setup.py
+++ setup.py
@@ -7,14 +7,22 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='pyaehw4a1',
+setup(
+    name='pyaehw4a1',
     version=pyaehw4a1.__version__,
     description='Python interface for Hisense AEH-W4A1 module',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/bannhead/pyaeh-w4a1',
+    url='https://github.com/bannhead/pyaehw4a1',
     author='Davide Varricchio',
     author_email='davide.varricchio@gmail.com',
     license='Apache 2.0',
-    packages=['pyaehw4a1'])
+    packages=['pyaehw4a1']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation",
+    ],
+)
 
